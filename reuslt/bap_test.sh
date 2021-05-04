09d9954c5a90146b4d386ceef9e3ab81df1a5131
Rungerlas <34901847+Rungerlas@users.noreply.github.com>
2021-05-04 00:11:45 -0400

    Add files via upload

---
+++ reuslt/bap_test.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# set -x
+
+# export DOCKER_REGISTRY='retrocontestviebtzaqpdksjflr.azurecr.io'
+# export docker_registry_username='xxx'
+# export docker_registry_password='xxx'
+
+# docker login $DOCKER_REGISTRY \
+#     --username $docker_registry_username \
+#     --password-stdin $docker_registry_password
+
+#!/usr/bin/env bash
+set -x
+
+export dir=dmr-agent
+
+rm -rf $dir
+mkdir $dir
+
+cp ../dockerfile/dmr.docker dmr-agent
+
+cp -r ../A3gent/lawking $dir
+cp -r ../A3gent/cpt $dir
+cp -r ../A3gent/detect $dir
+cp -r ../A3gent/detect_model $dir
+
+cp ../A3gent/dmr_agent.py $dir/agent.py
+
+export version='all2'
+#Add some tools
+cp ./apt-transport-https_1.2.27_amd64.deb $dir
+
+cd $dir
+docker build -f dmr.docker -t sonic/dmr-agent:$version .
+
+#docker push $DOCKER_REGISTRY/dmr-agent:$version
+
